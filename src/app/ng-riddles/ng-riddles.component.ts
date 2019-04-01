dc561d3bb9bb6e4c122eeafc42d05492b6db9c91
Yash <yasharya11@gmail.com>
2019-03-31 17:42:18 -0700

    Error Messages added

--- src/app/ng-riddles/ng-riddles.component.ts
+++ src/app/ng-riddles/ng-riddles.component.ts
@@ -13,6 +13,8 @@ export class NgRiddlesComponent implements OnInit {
     riddle: "How many cylinders are in a V8?",
     answer: "8",
     ansIsCorrect: "false",
+    hint: "Keep trying! The answer is within the question!",
+    ansAttButWrong: "false",
     nextLocation: "8800 Grossmont College Dr, El Cajon, CA 92020",
     showLoc: 'false'
   };
@@ -23,6 +25,8 @@ export class NgRiddlesComponent implements OnInit {
     riddle: "How many feet are in a quarter mile?",
     answer: "1320",
     ansIsCorrect: "false",
+    hint: "Keep trying! There is only one right answer!",
+    ansAttButWrong: "false",
     nextLocation: "United States Postal Office: 6030 Santo Rd Ste I, San Diego CA 92124",
     showLoc: 'false'
   };
@@ -33,13 +37,15 @@ export class NgRiddlesComponent implements OnInit {
     riddle: "I have keys but no doors, I have space but no room.. You can \"enter\" but you can't leave. What am I?",
     answer: "a keyboard",
     ansIsCorrect: "false",
-    nextLocation: "Pockets Billiards & Brew: 5610 Kearny Mesa Rd, Unit E, San Diego, CA 92111 [TURN LEFT ON MAGNETRON]",
+    hint: "Keep trying! Taking your time typing it out...",
+    ansAttButWrong: "false",
+    nextLocation: "Pockets Billiards & Brew: 5610 Kearny Mesa Rd, Unit E, San Diego, CA 92111",
     showLoc: 'false'
   };
 
   riddle1CodeCheck(givenCode)
   {
-    if (givenCode == this.riddle1.beginCode)
+    if (givenCode.trim() == this.riddle1.beginCode)
     {
       this.riddle1.bcIsCorrect = 'true';
     }
@@ -47,7 +53,8 @@ export class NgRiddlesComponent implements OnInit {
 
   riddle1AnsCheck(ans)
   {
-    if (ans == this.riddle1.answer)
+    this.riddle1.ansAttButWrong = 'true';
+    if (ans.trim() == this.riddle1.answer)
     {
       this.riddle1.ansIsCorrect = 'true';
     }
@@ -55,7 +62,7 @@ export class NgRiddlesComponent implements OnInit {
 
   riddle2CodeCheck(givenCode)
   {
-    if (givenCode == this.riddle2.beginCode)
+    if (givenCode.trim() == this.riddle2.beginCode)
     {
       this.riddle2.bcIsCorrect = 'true';
     }
@@ -63,7 +70,8 @@ export class NgRiddlesComponent implements OnInit {
 
   riddle2AnsCheck(ans)
   {
-    if (ans == this.riddle2.answer)
+    this.riddle2.ansAttButWrong = 'true';
+    if (ans.trim() == this.riddle2.answer)
     {
       this.riddle2.ansIsCorrect = 'true';
     }
@@ -71,7 +79,7 @@ export class NgRiddlesComponent implements OnInit {
 
   riddle3CodeCheck(givenCode)
   {
-    if (givenCode == this.riddle3.beginCode)
+    if (givenCode.trim() == this.riddle3.beginCode)
     {
       this.riddle3.bcIsCorrect = 'true';
     }
@@ -79,7 +87,9 @@ export class NgRiddlesComponent implements OnInit {
 
   riddle3AnsCheck(ans)
   {
-    if (ans.toLowerCase() == this.riddle3.answer.toLowerCase())
+    this.riddle3.ansAttButWrong = 'true';
+    ans = ans.trim().toLowerCase();
+    if (ans == this.riddle3.answer || ans == "keyboard" || ans == "im a keyboard" || ans == "i'm a keyboard" || ans == "you're a keyboard" || ans == "youre a keyboard" || ans == "you are a keyboard")
     {
       this.riddle3.ansIsCorrect = 'true';
     }
